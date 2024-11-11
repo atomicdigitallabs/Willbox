2fceaac06a41dc2daa9a52e74f3955fe0166ed74
Atomic Digital <Pablo@atomicdigitalmarketing.co.uk>
2024-11-11 18:45:21 +0700

    Update enquiry-form.js

--- enquiry-form.js
+++ enquiry-form.js
@@ -1,141 +1,119 @@
 "use strict";
-(() => {
-    var ze = Object.defineProperty;
-    var Ke = (a, e, t) => (e in a ? ze(a, e, { enumerable: true, configurable: true, writable: true, value: t }) : a[e] = t);
-    var _ = (a, e, t) => (Ke(a, typeof e !== "symbol" ? e + "" : e, t), t);
-    var xe = a => { var e = 0; for (var t in a) if (a.hasOwnProperty(t)) ++e; return e; };
-    var T = a => !Array.isArray(a) || !a.length;
-    var R = a => a instanceof Date && Object.prototype.toString.call(a) === "[object Date]" && !isNaN(a.getTime());
-    var Q = a => (a < 10 ? "0" + a : a);
 
-    var U = a => {
-        if (R(a)) return a.toLocaleString("en-GB", { day: "numeric", month: "numeric", year: "numeric" });
-        var e = new Date(a);
-        return e.toLocaleString("en-GB", { day: "numeric", month: "numeric", year: "numeric" });
-    };
+// Definición de funciones de configuración y manipulación del formulario
 
-    var _e = (a, e, t) => (a.splice(t, 0, a.splice(e, 1)[0]), a);
-    var re = a => !!(a.prop("checked") === true || a.is(":checked"));
-    
-    var V = a => {
-        var e = JSON.parse(localStorage.getItem("enquiry"));
-        if (!T(e)) {
-            let i = [];
-            for (let s = 0; s < e.length; s++) {
-                var r = e[s], n = {};
-                for (let d in r) {
-                    if (r.hasOwnProperty(d)) {
-                        if (d === "billing" || d === "name" || d === "variant" || d === "qty") {
-                            n[d] = r[d];
-                        } else if (d === "selectedDates") {
-                            if (a === true) {
-                                var l = JSON.parse(localStorage.getItem("selectedDates"));
-                                if (!T(l)) {
-                                    $.each(l, function (f, y) {
-                                        var c = new Date(y);
-                                        if (R(c)) {
-                                            var g = c.getFullYear(),
-                                                p = Q(c.getMonth() + 1),
-                                                h = Q(c.getDate());
-                                            f === 0 ? n.hireStart = h + "-" + p + "-" + g : n.hireEnd = h + "-" + p + "-" + g;
-                                        }
-                                    });
-                                }
-                            } else if (!T(r[d])) {
-                                var l = r[d];
-                                $.each(l, function (f, y) {
-                                    var c = new Date(y);
-                                    if (R(c)) {
-                                        var g = c.getFullYear(),
-                                            p = Q(c.getMonth() + 1),
-                                            h = Q(c.getDate());
-                                        f === 0 ? n.hireStart = h + "-" + p + "-" + g : n.hireEnd = h + "-" + p + "-" + g;
-                                    }
-                                });
-                            }
-                        }
-                    }
-                }
-                i.push(n);
-            }
-            $("#enquiry_data").val(JSON.stringify(i));
+function ke(reasonType) {
+    // Configura la redirección en función del tipo de motivo (New Hire o Sales Quote)
+    Se(reasonType === "NewHire" ? "/complete-your-enquiry/hire-thank-you" : "/complete-your-enquiry/sales-thank-you");
+
+    $("#upload-file-show-hide").addClass("hidden");
+    $("#tab-group,#placed-order-show-hide,#know-account-num-show-hide,#vat-registered-show-hide,#address,#items-show-hide").removeClass("hidden");
+
+    $("#addressline1_delivery,#postcode_delivery").prop("required", true).attr("required", true);
+    $("#label-is-a-company").html(reasonType === "NewHire" ? "Are you hiring for a company?*" : "Are you requesting a sales quote for a company?*");
+    $("#label-notes").html(reasonType === "NewHire" ? "Your hire message" : "Your sales message");
+    $("#notes").attr("placeholder", reasonType === "NewHire" ? "Enter additional info about your hire enquiry" : "Enter additional info about your sales enquiry");
+
+    // Establece el tipo de motivo en el formulario
+    $("#reason_for_enquiry").val(reasonType === "NewHire" ? "New Hire" : "Sales Quote");
+}
+
+function be() {
+    Se("/complete-your-enquiry/thank-you");
+    $("#addressline1_delivery,#postcode_delivery").prop("required", false).removeAttr("required");
+    $("#tab-group,#address,#items-show-hide").addClass("hidden");
+    $("#reason-for-enquiry-show-hide,#upload-file-show-hide").removeClass("hidden");
+
+    $("#label-is-a-company").html("Are you enquiring on behalf of a company?*");
+    $("#label-notes").html("Your project requirements");
+    $("#notes").attr("placeholder", "Please provide a brief summary of your project to discuss.");
+}
+
+function Je() {
+    Se("/complete-your-enquiry/thank-you");
+    $("#enquiry-title").html("Get in touch");
+    $("#addressline1_delivery,#postcode_delivery").prop("required", false).removeAttr("required");
+    $("#tab-group,#address,#upload-file-show-hide,#items-show-hide").addClass("hidden");
+    $("#reason-for-enquiry-show-hide").removeClass("hidden");
+
+    $("#label-is-a-company").html("Are you enquiring on behalf of a company?*");
+    $("#label-notes").html("Your message");
+    $("#notes").attr("placeholder", "Please provide a brief summary of your project to discuss.");
+}
+
+// Configura los motivos en el campo reason_for_enquiry
+$("#reason_for_enquiry").on("change", function() {
+    let selectedReason = $(this).val();
+
+    if (selectedReason === "Container Conversions" || selectedReason === "Self Storage") {
+        be();
+        if (selectedReason === "Container Conversions") {
+            he("reason", "ContainerConversions");
         } else {
-            $("#enquiry_data").val("");
+            he("reason", "SelfStorage");
         }
-    };
-
-    var Z = a => {
-        if (a === "complete-enquiry") {
-            var e = JSON.parse(localStorage.getItem("enquiry")), t = 0;
-            for (let i = 0; i < e.length; i++) t += parseInt(e[i].qty);
-            $('[data-enquiry="total"]').text(t);
-            $('[data-enquiry="get-a-quote"]').attr("href", "/complete-your-enquiry?reason=NewHireSalesQuote");
-        } else {
-            $('[data-enquiry="get-a-quote"]').attr("href", "/complete-your-enquiry");
+    } else if (selectedReason === "General Enquiry") {
+        Je();
+        he("reason", "GeneralEnquiry");
+    } else {
+        // Separa los casos para "New Hire" y "Sales Quote"
+        if (selectedReason === "New Hire") {
+            ke("NewHire");
+            he("reason", "NewHire");
+            $("#enquiry-title").html("Get a New Hire Quote");
+        } else if (selectedReason === "Sales Quote") {
+            ke("SalesQuote");
+            he("reason", "SalesQuote");
+            $("#enquiry-title").html("Get a Sales Quote");
         }
-        $('[data-enquiry="get-a-quote"] .' + a).show().siblings().hide();
-    };
+    }
+});
 
-    var H = a => {
-        return '<div class="datepicker-container"><div class="datepicker-title">' + a + '</div><div class="datepicker-panel" data-view="years picker"><ul><li data-view="years prev">&lsaquo;</li><li data-view="years current"></li><li data-view="years next">&rsaquo;</li></ul><ul data-view="years"></ul></div><div class="datepicker-panel" data-view="months picker"><ul><li data-view="year prev">&lsaquo;</li><li data-view="year current"></li><li data-view="year next">&rsaquo;</li></ul><ul data-view="months"></ul></div><div class="datepicker-panel" data-view="days picker"><ul><li data-view="month prev">&lsaquo;</li><li data-view="month current"></li><li data-view="month next">&rsaquo;</li></ul><ul data-view="week"></ul><ul data-view="days"></ul></div></div>';
-    };
+// Función para actualizar el estado del motivo en la URL
+function he(paramName, paramValue) {
+    paramName = encodeURI(paramName);
+    paramValue = encodeURI(paramValue);
+    let urlParams = document.location.search.substr(1).split("&");
+    let found = false;
 
-    $.fn.datepicker.setDefaults({
-        format: "dd-mm-yyyy",
-        language: "en-GB",
-        autoHide: true,
-        startDate: new Date(),
-        pick: function (a) {
-            if (a.view === "day") {
-                var e = $(this), t = e.attr("id");
-                if (t === "hire-start") {
-                    var i = a.date, s = a.date.getTime(), r = new Date(s + 864e5 * 28), n = $("#hire-end"), d = n.datepicker("getDate");
-                    n.datepicker("setStartDate", r);
-                    if (R(d)) {
-                        if (i >= new Date(d.getTime() - 864e5 * 28)) {
-                            n.datepicker("setDate", r);
-                            n.datepicker("show");
-                        }
-                    }
-                } else if (t === "hire-end") {
-                    var l = $("#hire-start");
-                    if (l.val() !== "") {
-                        var d = a.date, i = l.datepicker("getDate");
-                        if (d < i) {
-                            a.preventDefault();
-                            alert("End date cannot be before the start date");
-                            return;
-                        }
-                        if (d === i) {
-                            a.preventDefault();
-                            alert("Minimum hire period is 1 day");
-                            return;
-                        }
-                    } else {
-                        l.datepicker("show");
-                    }
-                }
-            }
+    for (let i = urlParams.length - 1; i >= 0; i--) {
+        let param = urlParams[i].split("=");
+        if (param[0] === paramName) {
+            param[1] = paramValue;
+            urlParams[i] = param.join("=");
+            found = true;
+            break;
         }
-    });
+    }
 
-    $("#submit-enquiry").on("click", function (a) {
-        var e = true, t = $("#reason_for_enquiry").val();
-        if (t === "Container Conversions" || t === "Self Storage" || t === "General Enquiry") {
-            var i = [
-                { id: "email_address", message: "Please enter an email address" },
-                { id: "phone_number", message: "Please enter a phone number" },
-                { id: "last_name", message: "Please enter a last name" },
-                { id: "first_name", message: "Please enter a first name" }
-            ];
-            $.each(i, function (g, p) {
-                $("#" + p.id).val()
-                    ? $("#" + p.id).siblings(".form-error").remove()
-                    : (e = false, $("#" + p.id).siblings(".form-error").length || $('<div class="form-error">' + p.message + "</div>").insertAfter("#" + p.id), $("#" + p.id).trigger("focus"));
-            });
-        } else {
-            // Additional form handling...
+    if (!found) {
+        urlParams[0] = paramName + "=" + paramValue;
+    }
+
+    let newUrl = urlParams.join("&");
+    if (history.replaceState) {
+        window.history.replaceState(null, null, window.location.pathname + "?" + newUrl);
+    }
+}
+
+// Configuración del campo reason_for_enquiry en función de parámetros de URL
+(function() {
+    let urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("reason")) {
+        let reasonParam = urlParams.get("reason");
+
+        if (reasonParam === "ContainerConversions" || reasonParam === "SelfStorage") {
+            be(true);
+            $("#reason_for_enquiry").val(reasonParam === "ContainerConversions" ? "Container Conversions" : "Self Storage");
+        } else if (reasonParam === "GeneralEnquiry") {
+            Je(true);
+            $("#reason_for_enquiry").val("General Enquiry");
+        } else if (reasonParam === "NewHire") {
+            ke("NewHire");
+            $("#reason_for_enquiry").val("New Hire");
+        } else if (reasonParam === "SalesQuote") {
+            ke("SalesQuote");
+            $("#reason_for_enquiry").val("Sales Quote");
         }
-        return e ? ($("#wf-form-enquiry").attr("data-name", t).attr("aria-label", t), $("#wf-form-enquiry").trigger("submit")) : false;
-    });
+    }
 })();
